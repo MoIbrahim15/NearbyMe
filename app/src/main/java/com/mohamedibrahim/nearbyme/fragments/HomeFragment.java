bc0192e9a78cf6229b684c1b0e0044081a9705d5
Mohamed Ibrahim <mohamed_ibrahim15@hotmail.com>
2016-10-30 03:06:20 +0200

    recycler_layout

--- app/src/main/java/com/mohamedibrahim/nearbyme/fragments/HomeFragment.java
+++ app/src/main/java/com/mohamedibrahim/nearbyme/fragments/HomeFragment.java
@@ -33,7 +33,7 @@ public class HomeFragment extends ParentFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, view);
         initTabs();
 
